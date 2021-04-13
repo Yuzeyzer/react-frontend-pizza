9975cf124895e484d5d70fc6755cae6703d78bd6
John Doe <kurbanof.atabek@gmail.com>
2021-04-13 13:35:47 +0600

    add Link to cart page

--- src/pages/cart/index.js
+++ src/pages/cart/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CartItem from '../../components/cartItem';
 import CartEmpty from '../../components/cartEmpty';
 import { useSelector, useDispatch } from 'react-redux';
@@ -121,7 +122,7 @@ const Cart = () => {
                 </span>
               </div>
               <div className='cart__bottom-buttons'>
-                <a href='/' className='button button--outline button--add go-back-btn'>
+                <Link to='/' className='button button--outline button--add go-back-btn'>
                   <svg
                     width='8'
                     height='14'
@@ -138,7 +139,7 @@ const Cart = () => {
                   </svg>
 
                   <span>Вернуться назад</span>
-                </a>
+                </Link>
                 <div className='button pay-btn'>
                   <span>Оплатить сейчас</span>
                 </div>
